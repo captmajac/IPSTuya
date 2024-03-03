4764d984c63c12742508c19a2fd75fd919f20034
captmajac <36032330+captmajac@users.noreply.github.com>
2024-03-03 19:43:27 +0100

    Update module.php

--- io/module.php
+++ io/module.php
@@ -3,7 +3,7 @@
 //Tuya Klassen einbinden
 include_once __DIR__ . "/../libs/TuyaAPI.php";
 
-class TuyaServer extends IPSModule
+class TuyaClient extends IPSModule
 {
     // erstellung
     public function Create()
