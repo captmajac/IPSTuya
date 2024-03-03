f534f5d036ebe665b1b7f14ddd9cffcd9fbbf7ee
captmajac <36032330+captmajac@users.noreply.github.com>
2024-03-03 12:17:55 +0100

    Update module.php

--- RGBWLED/module.php
+++ RGBWLED/module.php
@@ -7,12 +7,6 @@ class TuyaLEDRGBW extends TuyaGeneric
 		{
 			//Never delete this line!
 			parent::Create();
-			//$this->RegisterPropertyString("DeviceID", "");
-			//$this->RegisterPropertyString("DeviceName", "");
-			//$this->RegisterPropertyString("LocalKey", "");
-			
-			//Connect to available gateway
-			//$this->ConnectParent("{A52FEFE9-7858-4B8E-A96E-26E15CB944F7}");
 		}
     
 		public function ApplyChanges()
@@ -29,8 +23,6 @@ class TuyaLEDRGBW extends TuyaGeneric
       			$this->EnableAction("Intensity");	
      			$this->EnableAction("ColorTemperature");	
       			$this->EnableAction("Color");	
-			
-			//$this->SetReceiveDataFilter(".*\"DeviceID\":".(int)hexdec($this->ReadPropertyString("DeviceIDRet")).".*");
 		}
 		
 		/*
@@ -44,20 +36,7 @@ class TuyaLEDRGBW extends TuyaGeneric
 		*	$this->SendDataToParent(json_encode(Array("DataID" => "{B87AC955-F258-468B-92FE-F4E0866A9E18}", "Buffer" => $Text)));
 		*}
    		*/
-		
-		public function ReceiveData($JSONString)
-		{
-			$data = json_decode($JSONString);
-			$this->SendDebug("TuyaGatewayData", $JSONString, 0);
-			//  auswerten
-			$this->Process($data);
-			
-		}
-    
-		// daten auswerten
-		private function Process($Data)
-		{ 	
-		}
+	
 
 		public function RequestAction($Ident, $Value)
 		{
@@ -77,26 +56,6 @@ class TuyaLEDRGBW extends TuyaGeneric
 			// Neuen Wert in die Statusvariable schreiben, wird über die Rückmeldung korrigiert
 			SetValue($this->GetIDForIdent($Ident), $Value);
 		}
-		
-		protected function RegisterProfileFloat($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits)
-		{
-			if (!IPS_VariableProfileExists($Name))
-			{
-				IPS_CreateVariableProfile($Name, 2);
-			}
-			else
-			{
-				$profile = IPS_GetVariableProfile($Name);
-				if ($profile['ProfileType'] != 2)
-					throw new Exception("Variable profile type does not match for profile " . $Name);
-			}
-			IPS_SetVariableProfileIcon($Name, $Icon);
-			IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
-			IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
-			IPS_SetVariableProfileDigits($Name, $Digits);
-			
-		}
-
 		
 		protected function SendDebug($Message, $Data, $Format)
 		{
