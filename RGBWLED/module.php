c2f55350acd20cf79ef11d70a5c543569d549d29
captmajac <36032330+captmajac@users.noreply.github.com>
2024-03-10 10:42:17 +0100

    Update module.php

--- RGBWLED/module.php
+++ RGBWLED/module.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../Generic/module.php';  // Base Module.php
 class TuyaLEDRGBW extends TuyaGeneric
 	{
 		// range":["white","colour","scene","music"]}"
-		$cmodes = array(
+		private $cmodes = array(
 		    "white" => 0,
 		    "colour" => 1,
 		    "scene" => 2,
