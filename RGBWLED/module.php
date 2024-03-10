02f53b078c99d4c44652e43a05303e523afbe055
captmajac <36032330+captmajac@users.noreply.github.com>
2024-03-10 12:00:02 +0100

    Update module.php

--- RGBWLED/module.php
+++ RGBWLED/module.php
@@ -145,9 +145,9 @@ class TuyaLEDRGBW extends TuyaGeneric
 			$return = parent::updateState(); 
 			
 			// state
-			//$key = array_search('switch_led', array_column($return->result, 'code'));
-			//$state = "".$return->result[$key]->value;
-			//SetValue($this->GetIDForIdent("Power"), $state);
+			$key = array_search('switch_led', array_column($return->result, 'code'));
+			$state = "".$return->result[$key]->value;
+			SetValue($this->GetIDForIdent("Power"), $state);
 
 			//color modes
 			$key = array_search('work_mode', array_column($return->result, 'code'));
@@ -173,9 +173,9 @@ class TuyaLEDRGBW extends TuyaGeneric
 
 	
 	// timer aufruf,
-	public function TimerEvent() {
+	/*public function TimerEvent() {
 		$this->updateState();
-	} 
+	} */
 		
 		// int color to tuya hex value
 		private function colinttohex(int $intval)
