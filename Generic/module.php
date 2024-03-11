5cdaa5251abc44c42b513c881f76ea3c3f9c137b
captmajac <36032330+captmajac@users.noreply.github.com>
2024-03-11 20:10:29 +0100

    Update module.php

--- Generic/module.php
+++ Generic/module.php
@@ -52,8 +52,11 @@ class TuyaGeneric extends IPSModule
 	    
 	$this->SetTimerInterval("UpdateTimer", $Interval);		// $this->ReadPropertyInteger("Interval")
 
-	$instance = $this->GetConfigurationForParent();
-	IPS_LogMessage("Tuya","Interval=".$instance);
+	$instance = IPS_GetInstance($this->InstanceID);
+        $ret = IPS_GetConfiguration($instance['ConnectionID']);
+        $para = json_decode($ret);
+	    
+	IPS_LogMessage("Tuya","instance=".$para);
     }
 
     public function Send(string $Text)
