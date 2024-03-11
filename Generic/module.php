f3fb4ac8ed168d85df852bd9d08fb560228cdc8b
captmajac <36032330+captmajac@users.noreply.github.com>
2024-03-11 19:53:25 +0100

    Update module.php

--- Generic/module.php
+++ Generic/module.php
@@ -24,7 +24,6 @@ class TuyaGeneric extends IPSModule
 
 	$this->CreateVarProfileModus();    
         $this->RegisterPropertyString("DeviceID", "");
-        //$this->RegisterPropertyString("DeviceName", "");
         $this->RegisterPropertyString("LocalKey", "");
 
 	$this->RegisterTimer("UpdateTimer",0,$Module."_TimerEvent(\$_IPS['TARGET']);");    
